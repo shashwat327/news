b61b77c8d337d483295d8c42cd93783be398409d
shashwat khobragade <shashwat327@gmail.com>
2020-01-16 20:28:34 +0530

    fifebase init
    Signed-off-by: shashwat khobragade <shashwat327@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB59Z4cOrUE3M0kmc3c0bUXtlP0NI61H-M",
+    authDomain: "news-d572d.firebaseapp.com",
+    databaseURL: "https://news-d572d.firebaseio.com",
+    projectId: "news-d572d",
+    storageBucket: "news-d572d.appspot.com",
+    messagingSenderId: "832729516743",
+    appId: "1:832729516743:web:5447fdd7a3362503744ce2",
+    measurementId: "G-88ZVG118N2"
+  }
 };
